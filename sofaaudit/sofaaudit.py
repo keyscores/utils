0903a30a89ff2c0561b41bb7193598fe057c9dd3
Sujan Shakya <suzan.shakya@gmail.com>
2014-08-12 00:51:13 +0545

    dropped Provider column from Accrual.csv as it is not helpful

--- sofaaudit/sofaaudit.py
+++ sofaaudit/sofaaudit.py
@@ -136,7 +136,7 @@ df_comb['After tax']=df_comb['Net revenue']-df_comb['Tax']
 df_comb['Fee value']=df_comb['After tax']*df_comb[u'Comissão']
 df_comb['Royalty']=df_comb['After tax']-df_comb['Fee value']
 
-columns_accrual = ['month,year','Region','Rights Holder','Vendor Identifier','Product Type Identifier','Asset/Content Flavor','Net revenue','Royalty', 'Units', 'Tax', 'After tax', 'Fee value', 'Media', 'Encoding U$', 'Provider']
+columns_accrual = ['month,year','Region','Rights Holder','Vendor Identifier','Product Type Identifier','Asset/Content Flavor','Net revenue','Royalty', 'Units', 'Tax', 'After tax', 'Fee value', 'Media', 'Encoding U$']
 accrual_groupbycols = ['month,year','Vendor Identifier','Region','Rights Holder','Product Type Identifier','Asset/Content Flavor']
 
 df_accrual_revenue   = df_comb[columns_accrual].groupby(accrual_groupbycols)['Net revenue'].sum()
@@ -147,8 +147,7 @@ df_accrual_after_tax = df_comb[columns_accrual].groupby(accrual_groupbycols)['Af
 df_accrual_fee_value = df_comb[columns_accrual].groupby(accrual_groupbycols)['Fee value'].sum()
 df_accrual_media     = df_comb[columns_accrual].groupby(accrual_groupbycols)['Media'].sum()
 df_accrual_encoding  = df_comb[columns_accrual].groupby(accrual_groupbycols)['Encoding U$'].sum()
-df_accrual_provider  = df_comb[columns_accrual].groupby(accrual_groupbycols)['Provider'].sum()
-df_accrual = pd.DataFrame([df_accrual_revenue,df_accrual_royalty,df_accrual_units,df_accrual_tax,df_accrual_after_tax,df_accrual_fee_value,df_accrual_media,df_accrual_encoding,df_accrual_provider]).transpose()
+df_accrual = pd.DataFrame([df_accrual_revenue,df_accrual_royalty,df_accrual_units,df_accrual_tax,df_accrual_after_tax,df_accrual_fee_value,df_accrual_media,df_accrual_encoding]).transpose()
 df_accrual.to_csv(filename_accrual, encoding='utf-8')
 del df_accrual
 
