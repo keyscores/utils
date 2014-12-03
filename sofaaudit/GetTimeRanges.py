e7ff03587d58a28ab32328fbc83b0592a7befd93
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2014-12-03 15:31:36 -0500

    changes

--- sofaaudit/GetTimeRanges.py
+++ sofaaudit/GetTimeRanges.py
@@ -6,21 +6,21 @@ import numpy as np
 
 file = "input/DeParaSofaDigital.xlsx"
 
-df = pd.read_excel(file,sheetname="Titles")[['Vendor Identifier','Region','Rights Holder','Start Date','End Date']]
+df_ranges = pd.read_excel(file,sheetname="Titles")[['Vendor Identifier','Region','Rights Holder','Start Date','End Date']]
 
 #pivot the values into position using melt
-df = pd.melt(df, id_vars=['Vendor Identifier', 'Region','Rights Holder'], value_name='Months')
+df_ranges = pd.melt(df_ranges, id_vars=['Vendor Identifier', 'Region','Rights Holder'], value_name='Month')
 
 #needs to be a datetime index to be able to resample below
-df = df.set_index('Months')
+df_ranges = df_ranges.set_index('Month')
 
 #a function to resample an index
-f = lambda df: df.resample(rule='M', how='first')
+f = lambda df_ranges: df_ranges.resample(rule='MS', how='first')
 # apply the resample rule to each groupby level
-df = df.groupby(['Vendor Identifier','Region','Rights Holder']).apply(f)
+df_ranges = df_ranges.groupby(['Vendor Identifier','Region','Rights Holder']).apply(f)
 
 # format the output, and drop unnecessary columns
-df =df.drop(['Vendor Identifier','Region','Rights Holder','variable'], axis=1)
+df_ranges =df_ranges.drop(['Vendor Identifier','Region','Rights Holder','variable'], axis=1)
 
-#print df
-df.to_excel('out.xlsx', encoding='utf-8',merge_cells=False)
+#print df_ranges
+#df_ranges.to_excel('out.xlsx', encoding='utf-8',merge_cells=False)
