745b93d7c6a139a6a9c0aec7f61cd97c21476623
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2014-12-03 12:01:02 -0500

    time ranges cleanup

--- sofaaudit/GetTimeRanges.py
+++ sofaaudit/GetTimeRanges.py
@@ -4,22 +4,23 @@ import datetime
 import pandas as pd
 import numpy as np
 
-filename_apple="test.xlsx"
 file = "input/DeParaSofaDigital.xlsx"
 
-df1 = pd.read_excel(filename_apple)
 df = pd.read_excel(file,sheetname="Titles")[['Vendor Identifier','Region','Rights Holder','Start Date','End Date']]
-df = df[:3]
 
 #pivot the values into position using melt
 df = pd.melt(df, id_vars=['Vendor Identifier', 'Region','Rights Holder'], value_name='Months')
 
+#needs to be a datetime index to be able to resample below
 df = df.set_index('Months')
+
+#a function to resample an index
 f = lambda df: df.resample(rule='M', how='first')
+# apply the resample rule to each groupby level
 df = df.groupby(['Vendor Identifier','Region','Rights Holder']).apply(f)
 
-#print df.agg()
+# format the output, and drop unnecessary columns
 df =df.drop(['Vendor Identifier','Region','Rights Holder','variable'], axis=1)
 
-print df
+#print df
 df.to_excel('out.xlsx', encoding='utf-8',merge_cells=False)
